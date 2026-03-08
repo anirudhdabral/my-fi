1a0c724d1e5f2c51554b685f311c2f86a146a55c
Anirudh Dabral <anirudhdabral49@gmail.com>
2026-03-08 19:03:07 +0530

    refactor: update `useChat` hook import path to shared `hooks` directory

--- proxy.ts
+++ proxy.ts
@@ -12,7 +12,7 @@ const PUBLIC_PATHS = [
   "/robots.txt",
 ];
 
-export async function middleware(req: NextRequest) {
+export async function proxy(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
   if (
